a21ad9e0a1ce2936d92a980de010b3c4e13c4354
yrbane <yrbane@nethttp.net>
2025-12-03 13:01:39 +0100

    feat: integrate lunar/cli package for CLI infrastructure
    - Add lunar/cli as Composer dependency
    - Update CommandInterface to extend Lunar\Cli\CommandInterface
    - Update AbstractCommand to extend Lunar\Cli\AbstractCommand
    - Update ConsoleHelper to extend Lunar\Cli\Helper\ConsoleHelper
    - Update TableRenderer to extend Lunar\Cli\Helper\TableRenderer
    - Update Command attribute to extend Lunar\Cli\Attribute\Command
    - Refactor bin/console to use Lunar\Cli\Application
    
    Closes #11

--- src/Attribute/Command.php
+++ src/Attribute/Command.php
@@ -1,20 +1,24 @@
 <?php
 /**
+ * Attribut Command de compatibilite pour le framework.
+ * Alias vers l'attribut du package lunar/cli.
  *
  * @since 0.0.1
  * @link https://nethttp.net
- * @Author seb@nethttp.net
- *
- *
+ * @author seb@nethttp.net
  */
+declare(strict_types=1);
 
 namespace App\Attribute;
 
+use Lunar\Cli\Attribute\Command as LunarCommand;
+
+/**
+ * Attribut Command.
+ *
+ * Alias de compatibilite vers Lunar\Cli\Attribute\Command.
+ */
 #[\Attribute(\Attribute::TARGET_CLASS)]
-class Command
+class Command extends LunarCommand
 {
-    public function __construct(
-        public string $name,
-        public string $description = ''
-    ) {}
 }
--- src/Service/Command/CommandInterface.php
+++ src/Service/Command/CommandInterface.php
@@ -1,34 +1,23 @@
 <?php
 /**
+ * Interface de compatibilite pour les commandes du framework.
+ * Etend l'interface du package lunar/cli.
  *
  * @since 0.0.1
  * @link https://nethttp.net
- * @Author seb@nethttp.net
- *
- *
+ * @author seb@nethttp.net
  */
 declare(strict_types=1);
 
 namespace App\Service\Command;
 
+use Lunar\Cli\CommandInterface as LunarCommandInterface;
+
 /**
  * Interface CommandInterface.
  *
- * Représente la forme minimale d’une commande du framework.
+ * Alias de compatibilite vers Lunar\Cli\CommandInterface.
  */
-interface CommandInterface
+interface CommandInterface extends LunarCommandInterface
 {
-    /**
-     * Exécute la commande avec les arguments passés en CLI.
-     *
-     * @param string[] $args
-     *
-     * @return int Code de sortie (0 = succès, >0 = code d’erreur)
-     */
-    public function execute(array $args): int;
-
-    /**
-     * Retourne une aide détaillée pour la commande (utilisée par --help).
-     */
-    public function getHelp(): string;
 }
