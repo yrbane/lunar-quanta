c1fd5f072cc90833fc742505e5b1dfd7b7c8f5e7
yrbane <yrbane@nethttp.net>
2025-12-03 16:31:32 +0100

    fix: update public/index.php to use Lunar namespace
    - Change App\Service\Core\FrontController to Lunar\Service\Core\FrontController
    - Remove debug dump() call
    - Use __DIR__ for reliable path resolution

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Service\Core\FrontController;
+use Lunar\Service\Core\FrontController;
 
 $frontController = new FrontController();
-dump($_SERVER);
 $frontController->run();
